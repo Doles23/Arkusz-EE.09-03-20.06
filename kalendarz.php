c4de408be5b927633e1a2c30fe6fc46b531c2345
Doles23 <dolewski.patryk@wp.pl>
2021-10-25 15:42:35 +0200

    qwrefwsaseffsdf
    sdas

---
+++ kalendarz.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      <meta charset="UTF-8"> 
+      <meta http-equiv="X-UA-Compatible"  content="IE=edge">
+    <link rel="stylesheet" href="style5.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Mój kalendarz</title>
+</head>
+         <body>
+    <div class="jeden">
+    <img src="logo1.png" alt="moj kalendarz">
+    </div>
+    <div class="dwa">
+        <h1>KALENDARZ</h1>
+        <?php
+
+        ?>
+     </div>
+     <main>
+        <?php
+
+        ?>
+      </main>
+        <footer>
+          <?php
+
+        ?>
+        <form method="POST">
+            DODAJ WPIS: <input type="text" name="zzapisz">
+            <button type="submit">DODAJ</button>
+          </form>
+        <p>STRONE WYKONAŁ: 00000000000</p>
+              </footer>
+        </body>
+ </html>
